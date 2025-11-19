a2e62cab557f7ca9739a8990ae062056308a9284
Arinze Emmanuel <arinzemmanuel23@gmail.com>
2025-11-19 22:39:48 +0100

    update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
-import Routes from "./src/routes";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
-import { useCallback, useEffect, useState } from "react";
 import { AppContextProvider } from "./src/context/AppContext";
 import { Provider } from "react-redux";
 import "react-native-gesture-handler";
 import RootNavigator from "./src/navigation/RootNavigator";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import store from "./src/redux/store/configureStore";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
@@ -23,18 +21,6 @@ export default function App() {
     Poppins700: require("./assets/fonts/Poppins/Poppins-Bold.ttf"),
   });
 
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded || fontError) {
-  //     setTimeout(async () => {
-  //       await SplashScreen.hideAsync();
-  //     }, 3000);
-  //   }
-  // }, [fontsLoaded, fontError]);
-
-  // useEffect(() => {
-  //   onLayoutRootView();
-  // }, [onLayoutRootView]);
-
   if (!fontsLoaded) {
     return null;
   }
@@ -42,12 +28,14 @@ export default function App() {
     <Provider store={store}>
       <AppContextProvider>
         <GestureHandlerRootView style={{ flex: 1 }}>
-          <BottomSheetModalProvider>
-            <NavigationContainer onReady={() => SplashScreen.hideAsync()}>
-              {/* <Routes /> */}
-              <RootNavigator />
-            </NavigationContainer>
-          </BottomSheetModalProvider>
+          <SafeAreaProvider>
+            <BottomSheetModalProvider>
+              <NavigationContainer onReady={() => SplashScreen.hideAsync()}>
+                {/* <Routes /> */}
+                <RootNavigator />
+              </NavigationContainer>
+            </BottomSheetModalProvider>
+          </SafeAreaProvider>
         </GestureHandlerRootView>
       </AppContextProvider>
     </Provider>
